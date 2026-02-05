23db9f9b3bf2bce25d6618b2e719bb4c939e7c2f
PhDomX <116180354+PhDomDom@users.noreply.github.com>
2026-02-05 06:10:28 -0300

    skeleton attack point

--- Assets/Scripts/Enemy/AnimationControl.cs
+++ Assets/Scripts/Enemy/AnimationControl.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class AnimationControl : MonoBehaviour
 {
+    [SerializeField] private Transform attackPoint;
+    [SerializeField] private float radius;
+    [SerializeField] private LayerMask playerLayer;
+    
     private Animator anim;
     // Start is called before the first frame update
     void Start()
@@ -14,5 +18,21 @@ public class AnimationControl : MonoBehaviour
     public void PlayAnim(int value)
     {
         anim.SetInteger("transition", value);
+    }
+
+    public void Attack()
+    {
+        Collider2D hit = Physics2D.OverlapCircle(attackPoint.position, radius, playerLayer);
+
+        if (hit != null)
+        {
+            // Detecta colisão com o player
+            Debug.Log("bateu no player");
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireSphere(transform.position, radius);
     }
 }
